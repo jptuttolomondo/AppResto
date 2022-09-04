2a89e1beaf46e0f108e7cb1eca81c70acd1f5709
Juan Pablo Tuttolomondo <jptuttolomondo@hotmail.com>
2022-09-04 20:50:13 -0300

    mozo2

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import logo from './assets/undraw_conversation_re_c26v 1.png'
+import botonMas from'./assets/bxs-plus-circle 1.svg'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-body">
+      <div className="App-header">
+
+      </div>
+      <div className="App-titulo">
+      Aplicación para <br></br>
+      Restaurantes/Café
+        </div>
+<div className="App-subtitulo">
+Atención de mesas
+</div>
+<div className="App-tablaItems">
+</div>
+<div className="App-fecha">Fecha:</div>
+<div className="App-mesa">Mesa:</div>
+<div className="App-total">Total:</div>
+<div className="App-agregarPedido">Agregar Item</div>
+<div className="App-nuevaMesa">Nueva Mesa</div>
+<div className="App-mas"><img src={botonMas} /></div>
+<div className="App-logo"><img src={logo} /></div>
+
     </div>
   );
 }
